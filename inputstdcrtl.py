785d3db4e139e8edc5176575c5ff6f7c48a171a0
Guilherme Willian Castro <castro.ethng@gmail.com>
2024-08-28 08:29:21 -0300

    Add files via upload

---
+++ inputstdcrtl.py
@@ -0,0 +1,67 @@
+from pynput import keyboard
+from pynput.keyboard import Controller
+import keyboard as kb
+import sys
+import winsound as s
+import time as t
+
+s.Beep(700, 100)
+s.Beep(1100, 300)
+
+k = Controller()
+pressed = False
+
+def log ():
+    log = t.strftime("%D - %H:%M:%S | ")
+    return str(log)
+    
+def register_sound():
+    s.Beep(700, 40)
+    
+def value1():
+    print(log(),'Medir')
+    register_sound()
+def value2():
+    print(log(),'Tarar')
+    register_sound()
+def value3():
+    print(log(),'Zerar')
+    register_sound()
+
+def press(key):
+    global pressed
+    if kb.is_pressed("ctrl+1"):
+        #print('%sp' % key)
+        value1()  
+        pressed = True
+    if not pressed and key == (keyboard.Key.space and keyboard.Key.ctrl):
+        #print('%sp' % key)
+        value1()
+        pressed = True
+    if not pressed and key == keyboard.Key.f4:
+        #print('%sp' % key) 
+        pressed = True
+
+def release(key):
+    global pressed
+    if key == keyboard.Key.f2:
+        value1()
+        pressed = False # key is released
+    if key == keyboard.Key.f3:
+        #print('%sr' %key)
+        value2()
+        pressed = False # key is released
+    if key == keyboard.Key.f4:
+        #print('%sr' %key) 
+        value3()  
+        pressed = False # key is released
+    if key == keyboard.Key.f6:
+        print(log(),'Sair')
+        s.Beep(1100, 100)
+        s.Beep(700, 300)
+        sys.exit()
+
+with keyboard.Listener( on_press=press, on_release=release) as listener: 
+    listener.join()
+
+#
